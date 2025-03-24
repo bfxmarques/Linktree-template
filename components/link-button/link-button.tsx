1955f2c4350989cfcf5066f73c281543a879a882
bfxmarques <felipe06consentino@gmail.com>
2025-03-24 13:09:23 -0300

    placed lintree elaine carriel

--- components/link-button/link-button.tsx
+++ components/link-button/link-button.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { FaCopy } from 'react-icons/fa';
+import { GoArrowUpRight } from "react-icons/go";
 
 interface LinkButtonProps {
   children: React.ReactNode;
@@ -12,12 +13,13 @@ interface LinkButtonProps {
 export function LinkButton({ children, href }: LinkButtonProps) {
   return (
     <a
-      className="cursor-pointer bg-black bg-opacity-40 border-black border-2 mb-2 flex items-center justify-center py-2 w-full text-white rounded-2xl"
+      className="capitalize cursor-pointer bg-black bg-opacity-75 border-pink-500 border-2 mb-2 flex items-center justify-center py-2 w-full text-white rounded-2xl"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
     >
       {children}
+      <GoArrowUpRight className='ml-3' />
     </a>
   );
 }
@@ -35,17 +37,17 @@ const CopyButton = () => {
   const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
   return (
-    <div className="flex justify-end w-full"> {}
+    <div className="flex justify-end w-full"> { }
       <div className="flex flex-col items-end">
         <CopyToClipboard text={currentUrl} onCopy={handleCopy}>
           <button
-            className="bg-none text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-300"
+            className="bg-none text-black px-2 py-1 rounded hover:bg-green-500 transition duration-300"
           >
             {copied ? 'Link Copiado!' : <FaCopy />}
           </button>
         </CopyToClipboard>
         {copied && (
-          <span className="text-green-500 mt-2">URL copiada para a área de transferência!</span>
+          <span className="text-black font-normal mb-3">URL copiada para a área de transferência!</span>
         )}
       </div>
     </div>
